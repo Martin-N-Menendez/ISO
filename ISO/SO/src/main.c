3bf0b77adadafb90d02b59f0d891570cc08e1639
martin-n-menendez <menendez91@live.com.ar>
2019-04-02 23:10:08 -0300

    TEC1 prende LEDR

--- ISO/SO/src/main.c
+++ ISO/SO/src/main.c
@@ -5,22 +5,28 @@
 #include "os.h"
 /*==================[macros and definitions]=================================*/
 
-
-
-//#define STACK_SIZE 1024
+#define EJ1
 #define DELAY_MS 1000
 #define LED 2	//#define LED LEDR
 
-//typedef void *(*task_type)(void *);
+typedef enum {UP,DOWN} button_state;
+//typedef enum {TEC1,TEC2,TEC3,TEC4} button_idx;
+//typedef enum {LEDR,LEDG,LEDB,LED1,LED2,LED3} led_idx;
+typedef enum {OFF,ON} led_state;
 
 /*==================[internal data declaration]==============================*/
 
 /*==================[internal functions declaration]=========================*/
 
 static void initHardware(void);
-//static void pausems(uint32_t t);
+
+#ifdef EJ0
 static void * task1(void * param);
 static void * task2(void * param);
+static void * task3(void * param);
+#endif
+
+extern bool gpioRead( gpioMap_t pin );
 
 /*==================[internal data definition]===============================*/
 
@@ -30,8 +36,10 @@ static void * task2(void * param);
 
 uint32_t stack1[STACK_SIZE/4];
 uint32_t stack2[STACK_SIZE/4];
+uint32_t stack3[STACK_SIZE/4];
 
-uint32_t sp1,sp2;
+uint32_t led_on_time_tick;
+uint32_t launched = 0;
 
 uint32_t current_task;
 
@@ -45,13 +53,11 @@ static void initHardware(void)
 	NVIC_SetPriority(PendSV_IRQn, (1 << __NVIC_PRIO_BITS) -1);
 }
 
-/*==================[external functions definitibon]==========================*/
-
+#ifdef EJ0
 void * task1(void* arg)
 {
-
 	while(1){
-		Board_LED_Toggle(LED+1);
+		Board_LED_Toggle(LED1);
 		task_delay(200);
 	}
 	return NULL;
@@ -59,72 +65,71 @@ void * task1(void* arg)
 
 void * task2(void* arg)
 {
-
 	while(1){
-		Board_LED_Toggle(LED+3);
+		Board_LED_Toggle(LED2);
 		task_delay(300);
 	}
 	return NULL;
 }
 
-
-/*
-uint32_t get_next_context_2(uint32_t current_action)
+void * task3(void* arg)
 {
-	switch (current_task){
-		case WAITING:
-			// si release > ready
-			break;
-		case READY:
-			// si start > running
-			break;
-		case SUSPENDED:
-			// si activate > ready
-			break;
-		case RUNNING:
-			// si wait > waiting
-			// si preempt > ready
-			// si terminate > suspended
-			break;
-		default:
-			break;
-
-	return next_sp;
-}
-*/
-
-/*
-uint32_t get_next_context(uint32_t current_sp)
-{
-	uint32_t next_sp;
-
-	// Es mejor contar TICKS aca!
-
-	switch (current_task){
-		case 0:
-			next_sp = sp1;
-			current_task = 1;
-			break;
-		case 1:
-			sp1 = current_sp;
-			next_sp = sp2;
-			current_task = 2;
-			break;
-		case 2:
-			sp2 = current_sp;
-			next_sp = sp1;
-			current_task = 1;
-			break;
-		default:
-			while(1){
-				__WFI();
-			}
-			break;
+	while(1){
+		Board_LED_Toggle(LED3);
+		task_delay(400);
 	}
-	return next_sp;
+	return NULL;
 }
-*/
 
+#endif
+
+/*==================[external functions definition]==========================*/
+#ifdef EJ1
+void* button_task(void* taskParam)
+{
+
+	button_state state = UP;
+	uint32_t on_time;
+
+	while(1){
+		task_delay(10);
+
+		if(!gpioRead(36) && state == UP){
+			task_delay(20);
+			if(!gpioRead(36)){
+				state = DOWN;
+				on_time = get_tick_count();
+			}
+		}
+
+		if(gpioRead(36) && state == DOWN){
+			task_delay(20);
+			if(gpioRead(36)){
+				state = UP;
+				on_time = get_tick_count() - on_time;
+				led_on_time_tick = on_time;
+				launched = 1; //give
+			}
+		}
+	}
+}
+
+void* led_task(void* taskParam){
+	while(1){
+		while(launched == 0){
+			//__WFI();
+			task_delay(100);
+		}
+		launched = 0;
+		//Board_LED_Set(LED,ON);
+		led_set(LEDR,ON);
+		task_delay(led_on_time_tick);
+		//Board_LED_Set(LED,OFF);
+		led_set(LEDR,OFF);
+	}
+}
+
+#endif
 
 /* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
 int main(void){
@@ -132,15 +137,26 @@ int main(void){
 	/* ------------- INICIALIZACIONES ------------- */
 
 	os_init();
+
+	#ifdef EJ0
 	task_create(stack1,STACK_SIZE,task1,(void *)0x11223344);
 	task_create(stack2,STACK_SIZE,task2,(void *)0x55667788);
+	task_create(stack3,STACK_SIZE,task3,(void *)0x22446688);
+	#endif
+
+	#ifdef EJ1
+	task_create(stack1,STACK_SIZE,button_task,(void *)0x11223344);
+	task_create(stack2,STACK_SIZE,led_task,(void *)0x55667788);
+	#endif
+
 
 	initHardware(); /* Inicializar la placa */
 
-
 	while(1) {  /* ------------- REPETIR POR SIEMPRE ------------- */
-		Board_LED_Toggle(LED);
-		//pausems(DELAY_MS);
+		//Board_LED_Toggle(LEDB);
+		Board_LED_Set(LED+3,ON);
+		//task_delay(500);
+		//Board_LED_Set(LED,OFF);
 		__WFI();
 	}
 
--- ISO/SO/src/os.c
+++ ISO/SO/src/os.c
@@ -8,6 +8,16 @@
 uint32_t tick_count = 0;
 /*==================[internal functions declaration]=========================*/
 
+#define FUNC0						0x0				/** Pin function 0 */
+#define FUNC1						0x1				/** Pin function 1 */
+#define FUNC2						0x2				/** Pin function 2 */
+#define FUNC3						0x3				/** Pin function 3 */
+#define FUNC4						0x4				/** Pin function 4 */
+#define FUNC5						0x5				/** Pin function 5 */
+#define FUNC6						0x6				/** Pin function 6 */
+#define FUNC7						0x7				/** Pin function 7 */
+
+#define OFF 						0x0
 /*==================[internal data definition]===============================*/
 
 task_struct task_list[N_TASK];
@@ -15,6 +25,88 @@ uint32_t current_task;
 uint32_t task_list_idx;
 
 uint32_t stack_idle[STACK_SIZE/4];
+
+typedef struct{
+   int8_t port;
+   int8_t pin;
+} pinConfigLpc4337_t;
+
+typedef struct{
+   int8_t port;
+   int8_t pin;
+} gpioConfigLpc4337_t;
+
+typedef struct{
+	pinConfigLpc4337_t pin_name;
+	int8_t func;
+	gpioConfigLpc4337_t gpio;
+} pin_config_gpio_lpc_4337_t;
+
+
+
+const pin_config_gpio_lpc_4337_t gpio_pins_config[] = {
+
+   { {4, 1}, FUNC0, {2, 1} },   /*   0   CON1_36   T_FIL1           */
+   { {7, 5}, FUNC0, {3,13} },   /*   1   CON1_34   T_COL2           */
+
+   { {1, 5}, FUNC0, {1, 8} },   /*   2   CON1_39   T_COL0           */
+   { {4, 2}, FUNC0, {2, 2} },   /*   3   CON1_37   T_FIL2           */
+   { {4, 3}, FUNC0, {2, 3} },   /*   4   CON1_35   T_FIL3           */
+   { {4, 0}, FUNC0, {2, 0} },   /*   5   CON1_33   T_FIL0           */
+   { {7, 4}, FUNC0, {3,12} },   /*   6   CON1_31   T_COL1           */
+
+   { {3, 2}, FUNC4, {5, 9} },   /*   7   CON1_29   CAN_TD           */
+   { {3, 1}, FUNC4, {5, 8} },   /*   8   CON1_27   CAN_RD           */
+
+   { {2, 3}, FUNC4, {5, 3} },   /*   9   CON1_25   RS232_TXD        */
+   { {2, 4}, FUNC4, {5, 4} },   /*  10   CON1_23   RS232_RXD        */
+
+   { {6,12}, FUNC0, {2, 8} },   /*  11   CON2_40   GPIO8            */
+   { {6,11}, FUNC0, {3, 7} },   /*  12   CON2_38   GPIO7            */
+   { {6, 9}, FUNC0, {3, 5} },   /*  13   CON2_36   GPIO5            */
+   { {6, 7}, FUNC4, {5,15} },   /*  14   CON2_34   GPIO3            */
+   { {6, 4}, FUNC0, {3, 3} },   /*  15   CON2_32   GPIO1            */
+
+   { {4, 4}, FUNC0, {2, 4} },   /*  16   CON2_30   LCD1             */
+   { {4, 5}, FUNC0, {2, 5} },   /*  17   CON2_28   LCD2             */
+   { {4, 6}, FUNC0, {2, 6} },   /*  18   CON2_26   LCD3             */
+   { {4, 8}, FUNC4, {5,12} },   /*  19   CON2_24   LCDRS            */
+   { {4,10}, FUNC4, {5,14} },   /*  20   CON2_22   LCD4             */
+
+   { {1, 3}, FUNC0, {0,10} },   /*  21   CON2_18   SPI_MISO         */
+
+   { {1,20}, FUNC0, {0,15} },   /*  22   CON2_16   ENET_TXD1        */
+   { {1,18}, FUNC0, {0,13} },   /*  23   CON2_14   ENET_TXD0        */
+   { {1,17}, FUNC0, {0,12} },   /*  24   CON2_12   ENET_MDIO        */
+   { {1,16}, FUNC0, {0, 3} },   /*  25   CON2_10   ENET_CRS_DV      */
+   { {7, 7}, FUNC0, {3,15} },   /*  26   CON2_08   ENET_MDC         */
+   { {0, 1}, FUNC0, {0, 1} },   /*  27   CON2_06   ENET_TXEN        */
+   { {0, 0}, FUNC0, {0, 0} },   /*  28   CON2_04   ENET_RXD1        */
+
+   { {6,10}, FUNC0, {3, 6} },   /*  29   CON2_35   GPIO6            */
+   { {6, 8}, FUNC4, {5,16} },   /*  30   CON2_33   GPIO4            */
+   { {6, 5}, FUNC0, {3, 4} },   /*  31   CON2_31   GPIO2            */
+   { {6, 1}, FUNC0, {3, 0} },   /*  32   CON2_29   GPIO0            */
+
+   { {4, 9}, FUNC4, {5,13} },   /*  33   CON2_23   LCDEN            */
+
+   { {1, 4}, FUNC0, {0,11} },   /*  34   CON2_21   SPI_MOSI         */
+
+   { {1,15}, FUNC0, {0, 2} },   /*  35   CON2_09   ENET_RXD0        */
+
+
+   { {1, 0}, FUNC0, {0, 4} },   /* 36   TEC1    TEC_1                 */
+   { {1, 1}, FUNC0, {0, 8} },   /* 37   TEC2    TEC_2                 */
+   { {1, 2}, FUNC0, {0, 9} },   /* 38   TEC3    TEC_3                 */
+   { {1, 6}, FUNC0, {1, 9} },   /* 39   TEC4    TEC_4                 */
+
+   { {2, 0}, FUNC4, {5, 0} },   /* 43   LEDR    LED0_R                */
+   { {2, 1}, FUNC4, {5, 1} },   /* 44   LEDG    LED0_G                */
+   { {2, 2}, FUNC4, {5, 2} },   /* 45   LEDB    LED0_B                */
+   { {2,10}, FUNC0, {0,14} },   /* 40   LED1    LED1                  */
+   { {2,11}, FUNC0, {1,11} },   /* 41   LED2    LED2                  */
+   { {2,12}, FUNC0, {1,12} },   /* 42   LED3    LED3                  */
+};
 
 /*==================[external data definition]===============================*/
 
@@ -176,5 +268,41 @@ uint32_t get_tick_count(void){
 
 
 /*==================[external functions definition]==========================*/
+
+void gpio_obtain_pin_config( gpioMap_t pin, int8_t* pin_name_port,int8_t* pin_name_pin, int8_t* func, int8_t* gpio_port,int8_t* gpio_pin){
+
+	*pin_name_port = gpio_pins_config[pin].pin_name.port;
+	*pin_name_pin  = gpio_pins_config[pin].pin_name.pin;
+	*func          = gpio_pins_config[pin].func;
+	*gpio_port     = gpio_pins_config[pin].gpio.port;
+	*gpio_pin      = gpio_pins_config[pin].gpio.pin;
+
+}
+
+bool gpioRead( gpioMap_t pin ){
+
+	bool ret_val = OFF;
+
+	int8_t pin_name_port = 0;
+	int8_t pin_name_pin = 0;
+
+	int8_t func = 0;
+
+	int8_t gpio_port = 0;
+	int8_t gpio_pin = 0;
+
+
+	gpio_obtain_pin_config( pin, &pin_name_port, &pin_name_pin, &func, &gpio_port,&gpio_pin);
+
+	ret_val = (bool) Chip_GPIO_ReadPortBit( LPC_GPIO_PORT, gpio_port, gpio_pin);
+
+	return ret_val;
+}
+
+void led_set( uint8_t LEDNumber, bool On ){
+
+	Board_LED_Set(LEDNumber-LEDR,On);
+}
+
 
 /*==================[end of file]============================================*/
